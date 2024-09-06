84226211b3d44bb3566180462a700624c1fdb7e5
Garrett Sward <swardg@wfu.edu>
2024-09-06 08:56:10 -0400

    v0.2 of bulletin board page, with actual z-index layering (3 layers).

--- embed.php
+++ embed.php
@@ -1,14 +1,4 @@
 <?php
-include_once '/home/swardgsi/public_html/includes/head.php';
-
-$main_id = "1EsfHL3-RT7QGVAGxhx1Bs1rJ4gftY99RFvsl4wr3JoY";
-$override_id = "14cSUMuTyZrY7yDFxdQ_s_zxqy51MxQVsafOxQOz-aRY";
-$presentation_id = $main_id;
-$presentation_div_visibility = '';
-$alert_div_visibility = 'hidden';
-
-
-
 // $curl = curl_init('https://wakealert.wfu.edu/test_banner.json');
 $curl = curl_init('https://swardg.sites.wfu.edu/data/test_banner.json');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -25,42 +15,20 @@ if ($err) {
 	if (isset($response['alerts'])) {
 		$alerts = $response['alerts'];
 	} else {
-		echo "no wakealerts<br />";
-	}
-}
-
-$dismiss_alert = false;
-if (isset($_GET['alert'])) {
-	if ($_GET['alert'] == 0) {
-		$dismiss_alert = true;
-	}
-}
-
-$enable_wakealerts = false;
-if (count($alerts) > 0 and !($dismiss_alert)) {
-	foreach ($alerts as $alert) {
-		if ($alert['alert_code'] != 0) {
-			$enable_wakealerts = true;
-			break;
-		}
-	}
-} else {
-	echo "no wakealerts<br />";
-}
-
-if ($enable_wakealerts) {
-	$alert_div_visibility = '';
-	$presentation_div_visibility = 'hidden';
-} else if (isset($_GET['override'])) {
-	if ($_GET['override'] == 1) {
-		$presentation_id = $override_id;
+		echo "No wakealerts.<br />";
 	}
 }
 ?>
 
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
 <head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<title>Bulletin Board | Wake Forest School of Law</title>
+	<script src="https://code.jquery.com/jquery-latest.js"></script>
+	<?php include_once '/home/swardgsi/public_html/includes/head.php';?>
 	<style>
 		<?php
 		$alerts_html = array();
@@ -70,32 +38,27 @@ if ($enable_wakealerts) {
 		foreach ($alerts as $alert) {
 			if ($alert['alert_code'] == 1 or $alert['alert_code'] == 2) {
 				$class_name = "alert-$alert_number";
-				$alert_css = ".$class_name {";
-				
 
+				$alert_css = ".$class_name {";
 				$alert_css .= ' border: solid;';
 				$alert_css .= ' border-color: ' . $alert['alert_color'] . ';';
 				$alert_css .= ' border-width: thick; ';
 				$alert_css .= ' background-color: ' . $alert['override_background_color'] . ';';
-
 				$alert_css .= ' h1 {color: ' . $alert['override_text_color'] . ';}';
 				$alert_css .= ' a {color: ' . $alert['override_link_color'] . ' !important;}';
-
-				$alert_name = $alert['alert_name'];
-				$banner_text = $alert['banner_text'];
+				$alert_css .= '}';
+				$alerts_css[] = $alert_css;
 
 				$alert_string = "<div class='$class_name'>" .
 							"<div class='row'>" .
 							"<div class='columns medium-24'>" .
-							"<h1>$alert_name</h1>" .
+							"<h1>". $alert['alert_name'] ."</h1>" .
 							"</div></div>" .
 							"<div class='row'>" .
 							"<div class='columns medium-24'>" .
-							"<h1>$banner_text</h1>" .
+							"<h1>". $alert['banner_text'] ."</h1>" .
 							"</div></div></div>";
-				$alert_css .= '}';
 				$alerts_html[] = $alert_string;
-				$alerts_css[] = $alert_css;
 				$alert_number += 1;
 			}
 		}
@@ -103,16 +66,35 @@ if ($enable_wakealerts) {
 			echo $css;
 		}
 		?>
-		.responsive-google-slides {
+		.wakealerts-layer {
+			position: absolute;
+			z-index: 3; /* put wakealerts above slides and base layer */
+/*			top: 0;
+			left: 0;*/
+			width: 100%;
+			height: 100%;
+			opacity: 0.5;
+		}
+		.google-slides-layer {
 			position: relative;
 			padding-bottom: 56.25%; /* 16:9 Ratio, (9/16)*100% = 56.25% */
+			z-index: 2;
 			height: 0;
 			overflow: hidden;
+			opacity: 0.5;
+			iframe {
+				border: 0;
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+			}
 		}
-		.responsive-google-slides iframe {
-			border: 0;
+		.base-layer {
 			position: absolute;
-			top: 0;
+			z-index: 0;
+			top: 56px;
 			left: 0;
 			width: 100%;
 			height: 100%;
@@ -121,27 +103,42 @@ if ($enable_wakealerts) {
 </head>
 <body>
 <div class="container">
-		<div class='row'>
-			<div class='columns small-8'>
-				<p><a href="./embed.php?alert=1">Enable alerts</a></p>
-			</div>
-			<div class='columns small-8'>
-				<p><a href="./embed.php?main=1&alert=0">Main presentation</a></p>
-			</div>
-			<div class='columns small-8'>
-				<p><a href="./embed.php?override=1&alert=0">Override presentation</a></p>
-			</div>
+	<div class='row'>
+		<div class='columns small-8'>
+			<!-- I want to invoke a function to switch from 0 to 1 and vice versa. -->
+			<p><a href="./embed.php?toggle_alerts=1">toggle alerts</a></p>
 		</div>
-		<div class=<?=$alert_div_visibility?>>
-		<?php
+		<div class='columns small-8'>
+			<p><a href="./embed.php?toggle_slides=0&toggle_alerts=0">default slides</a></p>
+		</div>
+		<div class='columns small-8'>
+			<p><a href="./embed.php?toggle_slides=1&toggle_alerts=0">override presentation</a></p>
+		</div>
+	</div>
+	<div class="wakealerts-layer">
+	<?php
+	if (isset($_GET['toggle_alerts']) and $_GET['toggle_alerts'] == 1) {
 		foreach ($alerts_html as $alert) {
 			echo $alert;
 		}
-		?>
-		</div>
+	}
+	?>
 	</div>
-	<div class="responsive-google-slides <?=$presentation_div_visibility?>">
+	<div class="google-slides-layer">
+	<?php
+		$presentation_id = "1EsfHL3-RT7QGVAGxhx1Bs1rJ4gftY99RFvsl4wr3JoY";
+		$override_id = "14cSUMuTyZrY7yDFxdQ_s_zxqy51MxQVsafOxQOz-aRY";
+
+		if (isset($_GET['toggle_slides'])) {
+			if ($_GET['toggle_slides'] == 1) {
+				$presentation_id = $override_id;
+			}
+		}
+	?>
 		<iframe src="https://docs.google.com/presentation/d/<?=$presentation_id?>/embed?start=true&loop=true&delayms=10000&rm=minimal" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+	</div>
+	<div class="base-layer">
+		<img src="dunes-at-dusk.jpg" alt="Desert dunes at dusk">
 	</div>
 </div>
 </body>
